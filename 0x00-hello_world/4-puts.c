62f9e50b5659a1ab854ff454daa21893fd043472
MphoD <mphoramatlali@gmail.com>
2023-07-14 11:02:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle")	
