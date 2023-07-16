30ec7c9e9e9d2e35c5ea9b499f94861ede803b41
MphoD <mphoramatlali@gmail.com>
2023-07-16 07:55:57 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	int n = 97;
-	while(n <= 122)
+
+	while (n <= 122)
 	{
-		if(n == 101 || n == 113)
+		if (n == 101 || n == 113)
 		{
 			n++;
 			continue;
 		}
-		putchar (n);
+		putchar(n);
 		n++;
 	}
 	putchar('\n');
