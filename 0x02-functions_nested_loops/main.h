054a51bde2a614604d9c1b205622d9425e61f13e
MphoD <mphoramatlali@gmail.com>
2023-07-18 16:55:22 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
 int _putchar(char);
 void print_alphabet(void);
@@ -8,11 +8,10 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int c);
 int _abs(int n);
-in print_last_digit(int)
+int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void)
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
 
 #endif
