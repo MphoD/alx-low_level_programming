43e9d4b0db7e671f509bc3d821e9a682cb9f1970
MphoD <mphoramatlali@gmail.com>
2023-07-19 13:47:06 -0700

    5-sign.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,14 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: the character to be checked
+ * @c: the alphabet to check for
  * return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+return (1);
+else if
+	return (0);
 }
+
