7c4268be487cc90a4606ceac8c6ca551557f49c3
MphoD <mphoramatlali@gmail.com>
2023-07-27 11:53:42 -0700

    103-add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ void print_number(int n);
 char *leet(char *);
 void print_number(int n);
 void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
